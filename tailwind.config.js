9080dc1ef79c3cd7485998dbab16adde49e1688a
Dmitro-Front-End <Dmitro.Front.End.React@gmail.com>
2023-12-22 10:11:08 +0200

    added top of content and top nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors : {
+      bgInput : '#FCFCFD',
+      brColor : '#EFEFEF',
+      textIOnput : 400,
+    },
   },
   plugins: [],
 }
